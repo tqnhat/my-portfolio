00ce05aa6c8f27c73a468b111a7fc7d34924f10d
tqnhat <nhat12c@gmail.com>
2022-02-10 11:34:26 +0700

    update button text

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ class About extends Component{
                             <a
                             href="#projects"
                             className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                            See My Past Work
+                            My Past Work
                             </a>
                         </div>
                     </div>
